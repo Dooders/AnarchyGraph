d50d1f6797c23c0ebf679156a5d548a62414c22c
Chris Mangum <csmangum@gmail.com>
2024-06-29 14:08:56 -0700

    Refactor Node class to use Edges container
    - Updated Node class to accept an Edges container for managing edges.
    - Modified __init__ method to initialize edges with an Edges instance.
    - Updated __repr__ method to reflect changes in edge storage.
    - Removed redundant methods for edge management.

--- swarm/node.py
+++ swarm/node.py
@@ -2,24 +2,19 @@
 Node stores edges in a dictionary where the key is the other node's id and the 
 value is the Edge object that has the reference to the node.
 
-Node does not store the graph it belongs to??? Will it need it? Can it somehow 
-reference a parent from itself?
-
-Node doesn't know what edges comes from another node into it?
-
 TODO:
 - Edge container to make it a node component. Allows for node to have specialty edges
 """
 
 from typing import Any
 
-from swarm.edge import Edge
+from swarm.edge import Edges
 
 
 class Node:
     """
     A node is a self-contained and decentralized entity in a graph that stores
-    it's own data and edges.
+    its own data and edges.
 
     Parameters
     ----------
@@ -27,69 +22,15 @@ class Node:
         The unique identifier of the node.
     data : Any, optional
         The data to be stored in the node. Defaults to None.
-
-    Methods
-    -------
-    add_edge(node: Node) -> None:
-        Adds an edge to the node.
-    remove_edge(node: Node) -> None:
-        Removes an edge from the node.
-    get_edges(self) -> dict:
-        Returns the edges of the node.
-
-    TODO
-    ----
-    - Allow multiple edges between nodes, and a way to manage them
+    edges : Edges, optional
+        Dict-like object to store edges. Defaults to an empty Edges and can be
+        initialized with existing edges.
     """
 
-    def __init__(self, node_id: int, data: Any = None) -> None:
+    def __init__(self, node_id: int, data: Any = None, edges: Edges = None) -> None:
         self.node_id = node_id
         self.data = data
-        self._edges = {}
-
-    def add_edge(self, node: "Node", edge_type: str = "undirected") -> None:
-        """
-        Adds an edge to the node.
-
-        Parameters
-        ----------
-        other_node : Node
-            The node to connect to.
-        edge_type : str, optional
-            The type of the edge. Defaults to "undirected".
-        weight : int, optional
-            The weight of the edge. Defaults to 1.
-        """
-        if node.node_id not in self._edges:
-            self._edges[node.node_id] = Edge(node, edge_type=edge_type)
-            if edge_type == "directed":
-                node.add_edge(self, edge_type="directed")
-
-    def remove_edge(self, node: "Node") -> None:
-        """
-        Removes an edge from the node.
-
-        Parameters
-        ----------
-        other_node : Node
-            The node to disconnect from.
-        """
-        if node.node_id in self._edges:
-            edge = self._edges[node.node_id]
-            if edge.edge_type == "directed":
-                node.remove_edge(self)
-            del self._edges[node.node_id]
-
-    def edges(self) -> dict:
-        """
-        Returns the edges of the node.
-
-        Returns
-        -------
-        dict
-            The edges of the node.
-        """
-        return self._edges
+        self.edges = edges or Edges()
 
     def __repr__(self) -> str:
         """
@@ -100,7 +41,9 @@ class Node:
         str
             A string representation of the node.
         """
-        return f"Node({self.node_id}, Data: {self.data}, Edges: {list(self._edges.keys())})"
+        return (
+            f"Node({self.node_id}, Data: {self.data}, Edges: {list(self.edges.keys())})"
+        )
 
     def __eq__(self, other: "Node") -> bool:
         return self.node_id == other.node_id
