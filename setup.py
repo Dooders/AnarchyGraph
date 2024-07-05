b7a72e94cf6db25de94636fb08455f14ac97b91b
Chris Mangum <csmangum@gmail.com>
2024-07-04 22:38:10 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="anarchygraph",
-    version="0.1.7",
+    version="0.1.8",
     author="Chris Mangum",
     author_email="csmangum@gmail.com",
     description="A decentralized graph system to simulate agents in an artificial reality.",
